75c4f142117fb4abc2b0b5a94e26e54352a7c560
Adrian Ortega <aosw82@gmail.com>
2025-02-28 16:00:50 -0700

    Act nuevo cliente

--- netlify/functions/db.js
+++ netlify/functions/db.js
@@ -14,3 +14,57 @@ exports.handler = async function(event, context) {
     }),
   };
 };
+
+let clientes = [
+];
+
+
+exports.handler = async function(event, context) {
+  // Solo permitir POST requests
+  if (event.httpMethod === 'POST') {
+    try {
+      const body = JSON.parse(event.body); // Parsear el cuerpo de la solicitud JSON
+      const { nombre, email, telefono, empresa } = body; // Extraer el nombre enviado
+
+      // Validación: Si no se pasa el nombre, devolver error
+      /*
+      if (!nombre) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({ mensaje: 'El nombre es obligatorio' }),
+        };
+      }
+        */
+
+      // Crear un nuevo usuario con un ID único
+      const nuevoCliente = {
+        id: clientes.length + 1, // Asignar un nuevo ID
+        nombre: nombre,
+        email: email,
+        telefono: telefono,
+        empresa: empresa
+      };
+
+      // Insertar el nuevo cliente en el arreglo
+      clientes.push(nuevoCliente);
+
+      return {
+        statusCode: 201, // Código de éxito para creación (201)
+        body: JSON.stringify({
+          mensaje: 'Cliente insertado exitosamente',
+          clientes: nuevoCliente,
+        }),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ mensaje: 'Error al insertar el usuario', error: error.message }),
+      };
+    }
+  }
+
+  return {
+    statusCode: 405, // Método no permitido si no es POST
+    body: JSON.stringify({ mensaje: 'Método no permitido' }),
+  };
+};
