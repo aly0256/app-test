4fb101d13de8c3d194ebdc076b097fb2a2d367cd
Adrian Ortega <aosw82@gmail.com>
2025-02-28 13:53:21 -0700

    Permitir acceso

--- netlify/functions/db.js
+++ netlify/functions/db.js
@@ -1,6 +1,11 @@
 exports.handler = async function(event, context) {
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',  // Permitir todos los orígenes, puedes especificar tu dominio si lo prefieres
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',  // Métodos permitidos
+      'Access-Control-Allow-Headers': 'Content-Type',  // Cabeceras permitidas
+    },
     body: JSON.stringify({
       clientes: [
         { id: '6474', nombre: 'Alex', email: 'alex@gmail.com', telefono: '6562999924', empresa: 'borda2'},
