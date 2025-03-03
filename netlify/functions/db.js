b8601e87039de72b893611f678bfbc46b38aa4b6
Adrian Ortega <aosw82@gmail.com>
2025-03-03 11:44:52 -0700

    datos actu

--- netlify/functions/db.js
+++ netlify/functions/db.js
@@ -8,8 +8,11 @@ exports.handler = async function(event, context) {
     },
     body: JSON.stringify({
       clientes: [
-        { id: '6474', nombre: 'Alex', email: 'alex@gmail.com', telefono: '6562999924', empresa: 'borda2'},
-        { id: '5ae0', nombre: 'Juan Pablo', email: 'algunEmail@email.com', telefono: '6561', empresa: 'STRATTEC'}
+        { id: '1', nombre: 'Alex', email: 'alex@gmail.com', telefono: '6562999924', empresa: 'borda2'},
+        { id: '2', nombre: 'Juan Pablo', email: 'algunEmail@email.com', telefono: '6561', empresa: 'STRATTEC'},
+        { id: '3', nombre: 'Adrian ortega', email: 'aosw82@gmail.com', telefono: '6562999924', empresa: 'PIDA'},
+        { id: '4', nombre: 'Antonio Sanchez', email: 'algo@email.com', telefono: '4564654646', empresa: 'HP'},
+        { id: '5', nombre: 'Alfonso Bermudez', email: 'alfonso@hotmail.com', telefono: '111122334', empresa: 'DELPHI'}
       ]
     }),
   };
