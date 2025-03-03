434cf0878446a5683a0ae46d471e432801f115ac
Adrian Ortega <aosw82@gmail.com>
2025-03-03 11:40:22 -0700

    cambio

--- netlify/functions/db.js
+++ netlify/functions/db.js
@@ -14,26 +14,3 @@ exports.handler = async function(event, context) {
     }),
   };
 };
-
-
-exports.handler = async function(event, context) {
-  // Obtener el 'id' desde la URL (ruta dinámica)
-  const idCliente = event.path.split('/').pop();  // Extraer el último segmento de la ruta
-
-  // Buscar el cliente con el id proporcionado
-  const cliente = clientes.find(c => c.id === idCliente);
-
-  // Si no se encuentra el cliente, devolver error 404
-  if (!cliente) {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({ mensaje: 'Cliente no encontrado' })
-    };
-  }
-
-  // Si se encuentra el cliente, devolverlo
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ cliente })
-  };
-};
